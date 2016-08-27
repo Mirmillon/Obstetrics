c024ec8c66598af1eed584eaa6a639837d87ec92
Xavier Compagnion <xavier.compagnion@gmail.com>
2016-08-27 04:43:24 +0200

    Commencement du design fenetre principale

--- Echographie/MainWindow.xaml.cs
+++ Echographie/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Echographie
 {
@@ -24,5 +11,7 @@ namespace Echographie
         {
             InitializeComponent();
         }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e) {Close();}
     }
 }
