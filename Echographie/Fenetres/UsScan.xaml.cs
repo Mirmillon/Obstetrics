34889e24119d619b6bd9dc92404f6474ecf6bda6
Xavier Compagnion <xavier.compagnion@gmail.com>
2016-08-28 04:39:22 +0200

    Set binding et Get Binding en place. Set Binding marche pour 1T

--- Echographie/Fenetres/UsScan.xaml.cs
+++ Echographie/Fenetres/UsScan.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System.Windows;
-using Echographie.Utilitaires;
-using Echographie.RDMS;
 using System;
 using System.Windows.Controls;
+using System.Collections.Generic;
+using Echographie.Utilitaires;
+using Echographie.RDMS;
+using Echographie.Classes;
 
 namespace Echographie.Fenetres
 {
@@ -18,6 +20,8 @@ namespace Echographie.Fenetres
             new GestionComboBox().SetComboxReference(new DataBase().GetPregnancyKind(), comboBoxPregnancyKind, 0);
             new GestionComboBox().SetComboxReference(new DataBase().GetTwin(), comboBoxTwin, 0);
             new GestionComboBox().SetComboxReference(new DataBase().GetPregnancyUscKind(), comboBoxPregnancyUscKind, 1);
+
+            comboBoxPregnancyUscKind.SelectedIndex = 0;
         }
 
         #region BUTTON
@@ -46,12 +50,28 @@ namespace Echographie.Fenetres
                     SetFirstQuarter();
                     labelWeightEstimation.Visibility = Visibility.Collapsed;
                     comboBoxWeightEstimation.Visibility = Visibility.Collapsed;
+                    new GestionGrille().GridVisibilty(gridCentre, 0);//Demarre sur le 1T
+                    switch (comboBoxPregnancyKind.SelectedIndex)
+                    {
+                        case 0://Single
+                            SetBindingUnique1T();
+                            break;
+                        case 1://Twin
+                            SetBindingTwin1T();
+                            break;
+                        case 2://Multiple
+                            SetBindingTwin1T();
+                            break;
+                        default:
+                            break;
+                    }
                     break;
                 case 1:
                 case 2://2 et 3 Trimestre
                     SetSecondQuarter();
                     labelWeightEstimation.Visibility = Visibility.Visible;
                     comboBoxWeightEstimation.Visibility = Visibility.Visible;
+                    new GestionGrille().GridVisibilty(gridCentre, 1);//Demarre sur le 2T
                     break;
                 case 3://Croissance
                     SetSecondQuarter();
@@ -90,16 +110,61 @@ namespace Echographie.Fenetres
                     SetPregnancyUnique();
                     comboBoxTwin.Visibility = Visibility.Collapsed;
                     textBoxNumberFoetus.Visibility = Visibility.Collapsed;
+                    switch(comboBoxPregnancyUscKind.SelectedIndex)
+                    {
+                        case 0:
+                            SetBindingUnique1T();
+                            break;
+                        case 1:
+                        case 2:
+                            break;
+                        case 3:
+                        case 4:
+                        case 5:
+                        case 6:
+                            break;
+
+                    }
                     break;
                 case 1://Twin
                     SetPregnancyNonUnique();
                     comboBoxTwin.Visibility = Visibility.Visible;
                     textBoxNumberFoetus.Visibility = Visibility.Collapsed;
+                    switch (comboBoxPregnancyUscKind.SelectedIndex)
+                    {
+                        case 0:
+                            SetBindingTwin1T();
+                            break;
+                        case 1:
+                        case 2:
+                            break;
+                        case 3:
+                        case 4:
+                        case 5:
+                        case 6:
+                            break;
+
+                    }
                     break;
                 case 2://Multiple
                     SetPregnancyNonUnique();
                     comboBoxTwin.Visibility = Visibility.Collapsed;
                     textBoxNumberFoetus.Visibility = Visibility.Visible;
+                    switch (comboBoxPregnancyUscKind.SelectedIndex)
+                    {
+                        case 0:
+                            SetBindingTwin1T();
+                            break;
+                        case 1:
+                        case 2:
+                            break;
+                        case 3:
+                        case 4:
+                        case 5:
+                        case 6:
+                            break;
+
+                    }
                     break;
                 default:
                     SetPregnancyUnique();
@@ -147,7 +212,172 @@ namespace Echographie.Fenetres
 
         #endregion END TEXTBOX
 
+        #region LISTS
+
+        private List<TextBox> GetMandatoryTextBox(Grid g)
+        {
+            List<TextBox> listes = new List<TextBox>();
+            foreach (Control control in g.Children)
+            {
+                TextBox t = control as TextBox;
+                if (t is TextBox)
+                {
+                    if (t.BorderBrush.ToString() == "#FFFFFF00")
+                    {
+                        listes.Add(t);
+                    }
+                }
+            }
+            return listes;
+        }
+
+        private List<TextBox> GetTextBoxPoids(Grid g)
+        {
+            List<TextBox> listes = new List<TextBox>();
+            foreach (Control control in g.Children)
+            {
+                TextBox t = control as TextBox;
+                if (t is TextBox)
+                {
+                    if (t.Background.ToString() == "#FFFFFF00")
+                    {
+                        t.TextChanged += TextBoxTerme_TextChanged;
+                        listes.Add(t);
+                    }
+                }
+            }
+            return listes;
+        }
+
+        #endregion FIN LISTS
+
+        #region BINDINGS
+
+        private void SetBindingMorphology(Grid grid)
+        {
+            //Liste des elements du 1T
+            List<ElementAnatomique> elements = new List<ElementAnatomique>();
+            elements = new DataBase().GetElementsAnatomiques1T();
+            //Liaison DataContext pour chaque uniteData de la grid biometric 1T
+            //Le seul moyen en attendant les UserControl eest de slectionner les UnitData sur leur nom gridUD
+            //1.Liste des gridUnit
+            List<Grid> grids = new List<Grid>();
+            foreach (Panel control in grid.Children)
+            {
+                Grid g = control as Grid;
+                if (g is Grid)
+                {
+                    if (g.Background.ToString() == "#00FFFFFF")
+                    {
+                        grids.Add(g);
+                    }
+                }
+            }
+            //2.Affection du DataContext
+            for (int i = 0; i < grids.Count; ++i)
+            {
+                grids[i].DataContext = elements[i];
+            }
+        }
+
+        private void SetBindingBiometrics(Grid grid)
+        {
+            //Liste des elements du 1T
+            List<ElementBiometrique> elements = new List<ElementBiometrique>();
+            elements = new DataBase().GetElement1T();
+            //Liaison DataContext pour chaque uniteData de la grid biometric 1T
+            //Le seul moyen en attendant les UserControl eest de slectionner les UnitData sur leur nom gridUD
+            //1.Liste des gridUnit
+            List<Grid> grids = new List<Grid>();
+            foreach (Panel control in grid.Children)
+            {
+                Grid g = control as Grid;
+                if (g is Grid)
+                {
+
+                    if (g.Background.ToString() == "#00FFFFFF")
+                    {
+                        grids.Add(g);
+                    }
+                }
+            }
+            //2.Affection du DataContext
+            for (int i = 0; i < grids.Count; ++i)
+            {
+                grids[i].DataContext = elements[i];
+            }
+        }
+
+        private List<ElementBiometrique> GetBindingBiometrics(Grid grid)
+        {
+            //Liste des elements du 1T
+            List<ElementBiometrique> elements = new List<ElementBiometrique>();
+
+            //Liaison DataContext pour chaque uniteData de la grid biometric 1T
+            //Le seul moyen en attendant les UserControl eest de slectionner les UnitData sur leur nom gridUD
+            //1.Liste des gridUnit
+
+            foreach (Panel control in grid.Children)
+            {
+                Grid g = control as Grid;
+                if (g is Grid)
+                {
+                    if (g.Background.ToString() == "#00FFFFFF")
+                    //if (new GestionGrille().GridName(g, "gridUD"))
+                    {
+                        object o = g.DataContext;
+                        ElementBiometrique elt = o as ElementBiometrique;
+                        if (elt is ElementBiometrique)
+                        {
+                            elements.Add(elt);
+                        }
+                    }
+                }
+            }
+            return elements;
+        }
+
+        private List<ElementAnatomique> GetBindingAnatomie(Grid grid)
+        {
+            //Liste des elements du 1T
+            List<ElementAnatomique> elements = new List<ElementAnatomique>();
+
+            //1.Liste des gridUnit
+            foreach (Panel control in grid.Children)
+            {
+                Grid g = control as Grid;
+                if (g is Grid)
+                {
+                    if (g.Background.ToString() == "#00FFFFFF")
+                    {
+                        object o = g.DataContext;
+                        ElementAnatomique elt = o as ElementAnatomique;
+                        if (elt is ElementAnatomique)
+                        {
+                            elements.Add(elt);
+                        }
+                    }
+                }
+            }
+            return elements;
+        }
+        #endregion END BINDINGS
+
         #region METHODES LOCALES
+
+        private void SetBindingUnique1T()
+        {
+            SetBindingBiometrics(gridBiometricUnique1T);
+            SetBindingMorphology(gridMorphologyUnique1T);
+        }
+
+        private void SetBindingTwin1T()
+        {
+            SetBindingBiometrics(gridBiometricNonUnique1TTwin1);
+            SetBindingMorphology(gridMorphologyNonUnique1TTwin1);
+            SetBindingBiometrics(gridBiometricNonUnique1TTwin2);
+            SetBindingMorphology(gridMorphologyNonUnique1TTwin2);
+        }
 
         private void SetPregnancyUnique()
         {
@@ -199,7 +429,22 @@ namespace Echographie.Fenetres
 
         private void CheckBoxAnatomie_Click(object sender, RoutedEventArgs e)
         {
-
+            CheckBox cb = sender as CheckBox;
+            if (cb is CheckBox)
+            {
+                if (cb.IsChecked == true)
+                {
+                    cb.Content = "Checked";
+                }
+                else if (cb.IsChecked == false)
+                {
+                    cb.Content = "Not Checked";
+                }
+                else
+                {
+                    cb.Content = "To Check Again";
+                }
+            }
         }
     }
 }
