e3d9cfd0a9c77ae8c4359ab0a94403d69baba5df
Xavier Compagnion <xavier.compagnion@gmail.com>
2016-09-17 00:12:50 +0200

    Classe referenceCheck faite

--- Echographie/Classes/Parametres/ReferenceCheck.cs
+++ Echographie/Classes/Parametres/ReferenceCheck.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Echographie.Classes.Parametres
+﻿namespace Echographie.Classes.Parametres
 {
-    class ReferenceCheck
+    public class ReferenceCheck : Reference
     {
+        private bool? check;
+
+        public ReferenceCheck()
+        {
+            check = false;
+        }
+
+        public bool? Check
+        {
+            get
+            {
+                //return (check ?? false);
+                return check;
+            }
+            set  
+            {
+                check = value;
+                OnPropertyChanged("Check");
+            }
+        }
     }
 }
